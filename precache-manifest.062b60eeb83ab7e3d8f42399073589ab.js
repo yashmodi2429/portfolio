1e277494127cbdfb75357b35d88461455595629e
yashmodi2429 <yashmodi2429.ym@gmail.com>
2020-07-17 09:17:32 -0700

    Updates

---
+++ precache-manifest.062b60eeb83ab7e3d8f42399073589ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf839056738b25f83226e59f508fea7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "37dd50676eae90a52a9f",
+    "url": "/portfolio/static/css/main.5baa317b.chunk.css"
+  },
+  {
+    "revision": "24ec662f6061c93c235f",
+    "url": "/portfolio/static/js/2.051a9d9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.051a9d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dd50676eae90a52a9f",
+    "url": "/portfolio/static/js/main.829ecf1f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ab06fb08612bc42968569437653ba145",
+    "url": "/portfolio/static/media/Angular.ab06fb08.gif"
+  },
+  {
+    "revision": "0d8899f8c2c2612cd482974d70e11598",
+    "url": "/portfolio/static/media/ddps.0d8899f8.jpg"
+  },
+  {
+    "revision": "ba5a258ca493b4b455a042f91a6d804d",
+    "url": "/portfolio/static/media/hiet.ba5a258c.png"
+  },
+  {
+    "revision": "4ce3aaee807099dc39d29964816a8444",
+    "url": "/portfolio/static/media/javascript_cert.4ce3aaee.png"
+  },
+  {
+    "revision": "c520c41c37ab354f1fc5ffb0f8902098",
+    "url": "/portfolio/static/media/nodejs.c520c41c.png"
+  },
+  {
+    "revision": "9f085f37036d3f0f9b3e0f160bdb8206",
+    "url": "/portfolio/static/media/reactjs.9f085f37.gif"
+  }
+]);
